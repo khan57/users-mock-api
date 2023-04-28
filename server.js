f26e98b9a58cc9caa22d78f332636dbdc1c56e08
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-04-28 19:03:46 +0500

    user-porfiling-added

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const app = require("express")();
 
-app.get("/v1/users", (req, res) => {
-  return res.json([
-    {
-      fullName: "Haseeb ur rehman",
-      userId: 1,
-    },
-  ]);
+app.get("/v1/users", async (req, res) => {
+  let { pageSize = 10 } = req.query;
+  if (parseInt(pageSize) && parseInt(pageSize) > 100) {
+    pageSize = 100;
+  }
+  let users = await fetch(`https://api.github.com/users?per_page=${pageSize}`);
+  users = await users.json();
+
+  return res.json(users);
 });
 
 const PORT = process.env.PORT || 3001;
