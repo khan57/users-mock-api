e9f29a42306842f5a17a9114e4f344b616bb0de1
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-04-28 19:11:14 +0500

    data-modeling-done

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const app = require("express")();
 
 app.get("/v1/users", async (req, res) => {
-  let { pageSize = 10 } = req.query;
-  if (parseInt(pageSize) && parseInt(pageSize) > 100) {
-    pageSize = 100;
-  }
-  let users = await fetch(`https://api.github.com/users?per_page=${pageSize}`);
-  users = await users.json();
+  try {
+    let { pageSize = 10 } = req.query;
+    if (parseInt(pageSize) && parseInt(pageSize) > 100) {
+      pageSize = 100;
+    }
+    let users = await fetch(
+      `https://api.github.com/users?per_page=${pageSize}`
+    );
 
-  return res.json(users);
+    users = await users.json();
+
+    users = users.map((user) => {
+      const { login, avatar_url, id } = user;
+      return {
+        name: login,
+        email: `${login}@gmail.com`,
+        imageUrl: avatar_url,
+        id,
+      };
+    });
+
+    return res.json(users);
+  } catch (error) {
+    return res.json({ error: "Something went wrong" + error });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
