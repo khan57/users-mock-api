eaaace03fa88ae3296b19c1a37dd57c7ef770db4
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-05-04 17:49:40 +0500

    typeSafetyOFArrayAdded

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ app.use(cors());
 app.get("/v1/users", async (req, res) => {
   try {
     let { pageSize = 10 } = req.query;
-    if (parseInt(pageSize) && parseInt(pageSize) > 100) {
-      pageSize = 100;
+    if (parseInt(pageSize) && parseInt(pageSize) > 50) {
+      pageSize = 50;
     }
     let users = await fetch(
       `https://api.github.com/users?per_page=${pageSize}`
@@ -13,27 +13,30 @@ app.get("/v1/users", async (req, res) => {
 
     users = await users.json();
 
-    users = users.map((user) => {
-      // Generate a random day between 1 and 28
-      var day = Math.floor(Math.random() * 28) + 1;
+    users =
+      Array.isArray(users) && users.length > 0
+        ? users.map((user) => {
+            // Generate a random day between 1 and 28
+            var day = Math.floor(Math.random() * 28) + 1;
 
-      // Generate a random month between 1 and 12
-      var month = Math.floor(Math.random() * 12) + 1;
+            // Generate a random month between 1 and 12
+            var month = Math.floor(Math.random() * 12) + 1;
 
-      // Generate a random year between 1900 and 2023 (current year)
-      var year = Math.floor(Math.random() * (2023 - 1900 + 1)) + 1900;
+            // Generate a random year between 1900 and 2023 (current year)
+            var year = Math.floor(Math.random() * (2023 - 1900 + 1)) + 1900;
 
-      // Format the date as "day-month-year"
-      var dob = day + "-" + month + "-" + year;
-      const { login, avatar_url, id } = user;
-      return {
-        name: login,
-        email: `${login}@gmail.com`,
-        imageUrl: avatar_url,
-        id,
-        date_of_birth: dob,
-      };
-    });
+            // Format the date as "day-month-year"
+            var dob = day + "-" + month + "-" + year;
+            const { login, avatar_url, id } = user;
+            return {
+              name: login,
+              email: `${login}@gmail.com`,
+              imageUrl: avatar_url,
+              id,
+              date_of_birth: dob,
+            };
+          })
+        : [];
 
     return res.json(users);
   } catch (error) {
